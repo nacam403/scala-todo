f8e6ceb6597b95fd53401d33b1f51b85c59366b8
nacam403 <satnakam@gmail.com>
2017-09-27 23:55:03 +0900

    Add DAO trait

---
+++ scala-todo-core/src/main/scala/nacam403/todo/core/TodoDao.scala
@@ -0,0 +1,13 @@
+package nacam403.todo.core
+
+import scala.concurrent.Future
+
+trait TodoDao {
+
+  def list: Future[Seq[Todo]]
+  def get(id: Long): Future[Option[Todo]]
+  def create(description: String): Future[Long]
+  def update(todo: Todo): Future[Int]
+  def delete(id: Long): Future[Int]
+
+}
