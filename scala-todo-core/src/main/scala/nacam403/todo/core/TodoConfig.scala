00898f8eaaed5b4fdf6ad736f6a05fc05ea903d1
nacam403 <satnakam@gmail.com>
2017-09-28 23:00:38 +0900

    rename slick config file in order to avoid confusion with play config

--- scala-todo-core/src/main/scala/nacam403/todo/core/TodoConfig.scala
+++ scala-todo-core/src/main/scala/nacam403/todo/core/TodoConfig.scala
@@ -1,9 +1,10 @@
 package nacam403.todo.core
 
+import com.typesafe.config.ConfigFactory
 import slick.jdbc.H2Profile.api._
 
 object TodoConfig {
 
-  val db = Database.forConfig("h2mem")
+  val db = Database.forConfig("h2mem", ConfigFactory.load("slick"))
 
 }
