a0277a39f56aeb9b6396cd4cac82918f7a5093c4
nacam403 <satnakam@gmail.com>
2017-10-01 16:37:56 +0900

    refactor about mocking

--- scala-todo-rest-api/test/controllers/TodoControllerSpec.scala
+++ scala-todo-rest-api/test/controllers/TodoControllerSpec.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import nacam403.todo.core.{Todo, TodoDao}
-import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers._
 import org.mockito.Mockito._
 import org.scalatest.{FunSpec, Matchers}
 import org.scalatest.mockito.MockitoSugar
@@ -21,8 +21,7 @@ class TodoControllerSpec extends FunSpec with MockitoSugar with Matchers {
 
   describe("TodoController") {
     it("should create new todo") {
-      when(mockDao.create("new todo")).thenReturn(Future(1L))
-
+      when(mockDao.create("new todo")) thenReturn Future(1L)
       val request = FakeRequest().withJsonBody(Json.parse("""{"description":"new todo"}"""))
       val resultFuture = controller.create(request)
       status(resultFuture) shouldBe OK
@@ -40,13 +39,12 @@ class TodoControllerSpec extends FunSpec with MockitoSugar with Matchers {
     }
 
     it("should return todo list") {
-      when(mockDao.list).thenReturn(Future {
+      when(mockDao.list) thenReturn Future {
         Seq(
           new Todo(1, "Todo 1"),
           new Todo(2, "Todo 2")
         )
-      })
-
+      }
       val resultFuture = controller.list(FakeRequest())
       status(resultFuture) shouldBe OK
       val json = contentAsJson(resultFuture)
@@ -54,23 +52,21 @@ class TodoControllerSpec extends FunSpec with MockitoSugar with Matchers {
     }
 
     it("should return existence todo") {
-      when(mockDao.get(1)).thenReturn(Future {
+      when(mockDao.get(1)) thenReturn Future {
         Some(new Todo(1, "Todo 1", true))
-      })
-
+      }
       val resultFuture = controller.get(1)(FakeRequest())
       status(resultFuture) shouldBe OK
     }
 
     it("should return NotFound with non existence todo ID for get") {
-      when(mockDao.get(-1)).thenReturn(Future(None))
-
+      when(mockDao.get(-1)) thenReturn Future(None)
       val resultFuture = controller.get(-1)(FakeRequest())
       status(resultFuture) shouldBe NOT_FOUND
     }
 
     it("should update existence todo") {
-      when(mockDao.update(new Todo(1, "updated todo", true))).thenReturn(Future(1))
+      when(mockDao.update(new Todo(1, "updated todo", true))) thenReturn Future(1)
 
       val jsonRequest = FakeRequest().withJsonBody(Json.parse("""{"description":"updated todo", "done":true}"""))
       val resultFuture = controller.update(1)(jsonRequest)
@@ -78,22 +74,21 @@ class TodoControllerSpec extends FunSpec with MockitoSugar with Matchers {
     }
 
     it("should return NotFound with non existence todo ID for update") {
-      when(mockDao.update(ArgumentMatchers.any())).thenReturn(Future(0))
+      when(mockDao.update(any())) thenReturn Future(0)
       val jsonRequest = FakeRequest().withJsonBody(Json.parse("""{"description":"updated todo", "done":true}"""))
       val resultFuture = controller.update(-1)(jsonRequest)
       status(resultFuture) shouldBe NOT_FOUND
     }
 
     it("should delete existence todo") {
-      when(mockDao.delete(1)).thenReturn(Future(1))
+      when(mockDao.delete(1)) thenReturn Future(1)
 
       val resultFuture = controller.delete(1)(FakeRequest())
       status(resultFuture) shouldBe OK
     }
 
     it("should return NotFound with non existence todo ID for delete") {
-      when(mockDao.delete(-1)).thenReturn(Future(0))
-
+      when(mockDao.delete(-1)) thenReturn Future(0)
       val resultFuture = controller.delete(-1)(FakeRequest())
       status(resultFuture) shouldBe NOT_FOUND
     }
